90adaa5d1eb7327ab9691ff19cf191b8a171a4ca
salim <jimsky699@gmail.com>
2023-06-26 17:22:32 +0100

    update UserInfo component and put background on the formLayout

--- src/components/CreateShopForm/UserInfo.tsx
+++ src/components/CreateShopForm/UserInfo.tsx
@@ -8,10 +8,18 @@ interface Props {
 export default function UserInfo(props: Props) {
     const { shopDatas, setShopDatas } = props;
 
-    console.log(shopDatas.shopName);
-
     return (
         <div>
+            <label htmlFor="fullname">Nom complet</label>
+            <input
+                type="text"
+                id="fullname"
+                placeholder="mohamed salim"
+                value={shopDatas.fullname}
+                onChange={(e) => {
+                    setShopDatas({ ...shopDatas, fullname: e.target.value });
+                }}
+            />
             <label htmlFor="email">Email</label>
             <input
                 type="email"
@@ -21,20 +29,10 @@ export default function UserInfo(props: Props) {
                     setShopDatas({ ...shopDatas, email: e.target.value });
                 }}
             />
-            <label htmlFor="fullname">Nom complet</label>
-            <input
-                type="text"
-                id="fullname"
-                placeholder=""
-                value={shopDatas.fullname}
-                onChange={(e) => {
-                    setShopDatas({ ...shopDatas, fullname: e.target.value });
-                }}
-            />
             <label htmlFor="text">Mot de passe</label>
             <input
                 type="password"
-                placeholder=""
+                placeholder="⚫⚫⚫⚫⚫⚫⚫⚫⚫⚫⚫⚫"
                 value={shopDatas.password}
                 onChange={(e) => {
                     setShopDatas({ ...shopDatas, password: e.target.value });
@@ -43,7 +41,7 @@ export default function UserInfo(props: Props) {
             <label htmlFor="text">Téléphone</label>
             <input
                 type="tel"
-                placeholder=""
+                placeholder="### ## ## ## ##"
                 value={shopDatas.phone}
                 onChange={(e) => {
                     setShopDatas({ ...shopDatas, phone: e.target.value });
