09fb0cebff06084fa724b340353998345a68530f
salim <jimsky699@gmail.com>
2023-07-25 08:57:57 +0100

    make box "choise theme"

--- src/pages/CustomShop.tsx
+++ src/pages/CustomShop.tsx
@@ -1,4 +1,27 @@
+export interface Theme {
+    image: string;
+    name: string;
+    date: string;
+}
+
 export default function CustomShop() {
+    const themes: Theme[] = [
+        {
+            image: "thumb_current_theme.webp",
+            name: "Theme 1",
+            date: "2023-28-12",
+        },
+        {
+            image: "thumb_current_theme.webp",
+            name: "Theme 2",
+            date: "2023-28-12",
+        },
+        {
+            image: "thumb_current_theme.webp",
+            name: "Theme 3",
+            date: "2023-28-12",
+        },
+    ]
     return (
         <div className="custom_shop">
             <span>Théme</span>
@@ -16,16 +39,24 @@ export default function CustomShop() {
                     </div>
                 </div>
             </section>
-            <span>Choisissez un théme pour votre boutique</span>
             <section className="choise_theme">
-                <div className="card_theme">
-                    <img src="/images/thumb_current_theme.webp" alt="current_theme" />
-                    <div className="text">
-                        <div className="name">Name theme</div>
-                        <div className="date">2023-28-12</div>
-                    </div>
+                <span>Choisissez un théme pour votre boutique</span>
+                <div className="themes">
+                    {themes.map((theme: Theme, key: number) =>
+                        <div className="card_theme" key={key}>
+                            <div className="image">
+                                <img src={`/images/${theme.image}`} alt={theme.name} />
+                            </div>
+                            <div className="details">
+                                <h3 className="name">{theme.name}</h3>
+                                <p className="date">{theme.date}</p>
+                                <button className="button">
+                                    Choisir
+                                    <i className='fa fa-check'></i>
+                                </button>
+                            </div>
+                        </div>)}
                 </div>
-                <button className="button">Customiser <i className="fa fa-paint-brush"></i></button>
             </section>
         </div>
     )
