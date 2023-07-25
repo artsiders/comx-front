be83b5df0f759d1ce5f5ca5b4e656c9d9838bca4
salim <jimsky699@gmail.com>
2023-07-25 08:23:52 +0100

    make current_theme design on customShop page

--- src/pages/CustomShop.tsx
+++ src/pages/CustomShop.tsx
@@ -1,7 +1,32 @@
 export default function CustomShop() {
     return (
-        <>
-            custom shop
-        </>
+        <div className="custom_shop">
+            <span>Théme</span>
+            <section className="current_theme">
+                <div className="cover" style={{ backgroundImage: 'url(/images/theme-cover.webp)' }}>
+                    <div>
+                        <div className="card_theme">
+                            <img src="/images/thumb_current_theme.webp" alt="current_theme" />
+                            <div className="text">
+                                <div className="name">Name theme</div>
+                                <div className="date">2023-28-12</div>
+                            </div>
+                        </div>
+                        <button className="button">Customiser <i className="fa fa-paint-brush"></i></button>
+                    </div>
+                </div>
+            </section>
+            <span>Choisissez un théme pour votre boutique</span>
+            <section className="choise_theme">
+                <div className="card_theme">
+                    <img src="/images/thumb_current_theme.webp" alt="current_theme" />
+                    <div className="text">
+                        <div className="name">Name theme</div>
+                        <div className="date">2023-28-12</div>
+                    </div>
+                </div>
+                <button className="button">Customiser <i className="fa fa-paint-brush"></i></button>
+            </section>
+        </div>
     )
 }
